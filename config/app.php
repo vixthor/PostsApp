64b0018a2d4f06559520e49aa0aca747f47fd070
vixthor <tega2112@gmail.com>
2023-01-21 23:10:36 +0100

    update

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Barryvdh\Debugbar\ServiceProvider::class,
+        // Barryvdh\Debugbar\ServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
